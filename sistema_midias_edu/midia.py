473ef271c5e7bd71fdb24d9286fd7379c0ff8b83
kevilyoliveira <kevily92@gmail.com>
2026-05-28 16:07:46 -0400

    Update midia.py

--- sistema_midias_edu/midia.py
+++ sistema_midias_edu/midia.py
@@ -1,1 +1,19 @@
+# SISTEMA 1 — Mídias Educacionais
 
+## midia.py
+
+```python id="69o9za"
+from abc import ABC, abstractmethod
+
+class Midia(ABC):
+    def __init__(self, titulo, duracao):
+        self.titulo = titulo
+        self.duracao = duracao
+
+    def mostrar_info(self):
+        print(f"Título: {self.titulo}")
+        print(f"Duração: {self.duracao}")
+
+    @abstractmethod
+    def reproduzir(self):
+        pass
