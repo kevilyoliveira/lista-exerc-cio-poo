93d95d7a36b7e7722aa807fa1eb607dd3277d8ed
kevilyoliveira <kevily92@gmail.com>
2026-05-28 16:12:04 -0400

    Create podcast.py

---
+++ sistema_midias_edu/podcast.py
@@ -0,0 +1,9 @@
+from midia import Midia
+
+class Podcast(Midia):
+    def __init__(self, titulo, duracao, apresentador):
+        super().__init__(titulo, duracao)
+        self.apresentador = apresentador
+
+    def reproduzir(self):
+        print(f"Reproduzindo podcast '{self.titulo}' com {self.apresentador}")
