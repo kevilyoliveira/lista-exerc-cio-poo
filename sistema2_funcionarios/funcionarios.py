5379e92865b2327c1d2ddd1045a9de8a22c3521d
kevilyoliveira <kevily92@gmail.com>
2026-05-29 08:56:48 -0400

    Update funcionarios.py

--- sistema2_funcionarios/funcionarios.py
+++ sistema2_funcionarios/funcionarios.py
@@ -1,1 +1,14 @@
+from abc import ABC, abstractmethod
 
+class Funcionario(ABC):
+    def __init__(self, nome, cpf):
+        self.nome = nome
+        self.cpf = cpf
+
+    def mostrar_dados(self):
+        print(f"Nome: {self.nome}")
+        print(f"CPF: {self.cpf}")
+
+    @abstractmethod
+    def calcular_pagamento(self):
+        pass
